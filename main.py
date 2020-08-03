d5a9dc865e069a703be64f03dc6d09c0fac66c21
TriRetr0 <63619167+TriRetr0@users.noreply.github.com>
2020-08-04 00:23:27 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QFileDialog
 import mainGUI
 
 
@@ -15,7 +16,7 @@ class MyWindow(QtWidgets.QMainWindow):
         QtWidgets.QMainWindow.__init__(self, parent)
         self.ui = mainGUI.Ui_Form()
         self.ui.setupUi(self)
-        self.setWindowIcon(QIcon('WFS.png'))
+        self.setWindowIcon(QIcon('./meta/WFS.png'))
         self.setWindowTitle('Wii U File System Qt')
 
         self.ui.pushButton.clicked.connect(self.mount_action)
@@ -24,22 +25,32 @@ class MyWindow(QtWidgets.QMainWindow):
         self.ui.pushButton_2.clicked.connect(self.action_2)
         self.ui.pushButton_5.clicked.connect(self.action_5)
         self.ui.pushButton_6.clicked.connect(self.action_6)
+        self.ui.toolButton.clicked.connect(self.openFile)
+        self.ui.toolButton_4.clicked.connect(self.openFile_4)
+        self.ui.toolButton_3.clicked.connect(self.openFile_3)
+        self.ui.toolButton_2.clicked.connect(self.openFile_2)
+        self.ui.toolButton_5.clicked.connect(self.openFile_5)
+        self.ui.toolButton_6.clicked.connect(self.openFile_6)
         self.ui.checkBox.toggled['bool'].connect(self.mlc)
         self.ui.checkBox_2.toggled['bool'].connect(self.device)
+        self.ui.label_13.setText("V1.1")
 
     def mount_action(self):
         seepromFile = self.ui.lineEdit.text()
+        if self.ui.checkBox.isChecked() == False :
+            MLC = "--usb"
+            seepromOPT = (f'--seeprom {seepromFile}')
+            print (seepromOPT)
+        else:
+            MLC = "--mlc"
+            seepromOPT = ("")
+
         otpFile = self.ui.lineEdit_2.text()
         imgFile = self.ui.lineEdit_3.text()
         pathFile = self.ui.lineEdit_5.text()
         mntWre = self.ui.lineEdit_7.text()
         PassWord = self.ui.lineEdit_8.text()
-        if self.ui.checkBox.isChecked() == False :
-            MLC = "--usb"
-        else:
-            MLC = "--mlc"
-        print(f'seeprom : {seepromFile}')
-        os.system(f'echo {PassWord} | sudo -S ./wfs-fuse {imgFile} {mntWre} --otp {otpFile} --seeprom {seepromFile} -o allow_other {MLC}')
+        os.system(f'echo {PassWord} | sudo -S ./wfslib/wfs-fuse {imgFile} {mntWre} --otp {otpFile} {seepromOPT} -o allow_other {MLC} && echo Done!')
 
     def action_4(self):
         webbrowser.open("https://discord.gg/PynXrnU")
@@ -60,7 +71,8 @@ class MyWindow(QtWidgets.QMainWindow):
         imgFile = self.ui.lineEdit_3.text()
         ExtFile = self.ui.lineEdit_6.text()
         OutDir = self.ui.lineEdit_9.text()
-        os.system(f'./wfs-extract --input {imgFile} --otp {otpFile} --seeprom {seepromFile} --output {OutDir} --dump-path {ExtFile} {MLC}')
+        
+        os.system(f'./wfslib/wfs-extract --input {imgFile} --otp {otpFile} --seeprom {seepromFile} --output {OutDir} --dump-path {ExtFile} {MLC}')
 
     def action_2(self):
         if self.ui.checkBox.isChecked() == False :
@@ -72,7 +84,8 @@ class MyWindow(QtWidgets.QMainWindow):
         imgFile = self.ui.lineEdit_3.text()
         injFile = self.ui.lineEdit_4.text()
         injPath = self.ui.lineEdit_5.text()
-        os.system(f'./wfs-file-injector --image {imgFile} --inject-file {injFile} --inject-path {injPath} --otp {otpFile} --seeprom {seepromFile} {MLC}')
+        
+        os.system(f'./wfslib/wfs-file-injector --image {imgFile} --inject-file {injFile} --inject-path {injPath} --otp {otpFile} --seeprom {seepromFile} {MLC}')
 
     def mlc(self):
         if self.ui.checkBox.isChecked() == False :
@@ -91,6 +104,40 @@ class MyWindow(QtWidgets.QMainWindow):
         else:
             self.ui.label_3.setText("Image")
             self.ui.lineEdit_3.setText("")
+
+    def openFile(self):
+        filename = QFileDialog.getOpenFileName(self, 'Select seeprom file')
+        seepromFile = filename[0]
+        print(seepromFile)
+        self.ui.lineEdit.setText(f"'{seepromFile}'")
+
+    def openFile_2(self):
+        filename_2 = QFileDialog.getOpenFileName(self, 'Select otp file')
+        otpFile = filename_2[0]
+        print(otpFile)
+        self.ui.lineEdit_2.setText(f"'{otpFile}'")
+
+    def openFile_3(self):
+        filename_3 = QFileDialog.getOpenFileName(self, 'Select file to inject')
+        injFile = filename_3[0]
+        print(injFile)
+        self.ui.lineEdit_4.setText(f"'{injFile}'")
+
+    def openFile_4(self):
+        filename_4 = QFileDialog.getOpenFileName(self, 'Select disk image file')
+        imgFile = filename_4[0]
+        print(imgFile)
+        self.ui.lineEdit_3.setText(f"'{imgFile}'")
+
+    def openFile_5(self):
+        mntWre = QFileDialog.getExistingDirectory(self, 'Select mount directory')
+        print(mntWre)
+        self.ui.lineEdit_7.setText(f"'{mntWre}'")
+
+    def openFile_6(self):
+        OutDir = QFileDialog.getExistingDirectory(self, 'Select output directory')
+        print(OutDir)
+        self.ui.lineEdit_9.setText(f"'{OutDir}'")
 
 if __name__ == '__main__':
     import sys
